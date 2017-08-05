1f6119aa272252ca43a97083a1ee729f3e654528
Alexander Lyon <arlyon@me.com>
2017-08-05 16:03:12 +0200

    bump version and fix client/serverside problems

--- src/main/java/arlyon/felling/proxy/ProxyClient.java
+++ src/main/java/arlyon/felling/proxy/ProxyClient.java
@@ -1,14 +1,27 @@
 package arlyon.felling.proxy;
 
+import arlyon.felling.Enchantment;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.enchantment.EnumEnchantmentType;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+import java.util.Arrays;
 
 public class ProxyClient extends ProxyCommon {
 
     @Override
     public void preInit(FMLPreInitializationEvent e) {
+
         super.preInit(e);
+
+        // add it to the creative tab
+        EnumEnchantmentType[] enchantmentTypes = CreativeTabs.TOOLS.getRelevantEnchantmentTypes();
+        enchantmentTypes = Arrays.copyOf(enchantmentTypes, enchantmentTypes.length+1);
+        enchantmentTypes[enchantmentTypes.length-1] = Enchantment.AXE;
+        CreativeTabs.TOOLS.setRelevantEnchantmentTypes(enchantmentTypes);
+
     }
 
     @Override
