53067cd3e850c9a4942cc1d69eaf710b0db5adde
Alexander Lyon <arlyon@me.com>
2017-08-09 21:10:19 +0200

    bump version number

--- src/main/java/arlyon/felling/Constants.java
+++ src/main/java/arlyon/felling/Constants.java
@@ -9,7 +9,7 @@ import java.util.Map;
 public class Constants {
     public static final String MOD_NAME = "Felling";
     public static final String MODID = "felling";
-    public static final String VERSION = "1.2.3";
+    public static final String VERSION = "1.3.0";
     public static final Enchantment felling = new Enchantment(net.minecraft.enchantment.Enchantment.Rarity.UNCOMMON, EntityEquipmentSlot.MAINHAND);
 
     public static Map<Integer, PlayerSettings> playerSettings = new HashMap<>();
