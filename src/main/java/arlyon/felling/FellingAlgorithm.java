7e19893ff8411afc9201895a0320a51e4e6cc62c
arlyon <arlyon@me.com>
2018-04-16 21:59:41 +0100

    Clean up and fix bug
    closes #16

--- src/main/java/arlyon/felling/FellingAlgorithm.java
+++ src/main/java/arlyon/felling/FellingAlgorithm.java
@@ -4,14 +4,12 @@ import arlyon.felling.util.ValueUniqueQueue;
 import net.minecraft.block.Block;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 import java.util.Arrays;
-import java.util.Random;
 
 import static net.minecraft.init.Enchantments.FORTUNE;
 
@@ -34,58 +32,58 @@ public class FellingAlgorithm {
      * The paths for felling one, to reach up and down.
      */
     private static final EnumFacing[][] firstTierPaths = {
-            // up and down
-            {EnumFacing.UP},
-            {EnumFacing.DOWN}
+        // up and down
+        {EnumFacing.UP},
+        {EnumFacing.DOWN}
     };
 
     /**
      * The paths for felling two, to reach all adjacent blocks.
      */
     private static final EnumFacing[][] secondTierPaths = {
-            // adjacent
-            firstTierPaths[0],
-            firstTierPaths[1],
-            {EnumFacing.NORTH},
-            {EnumFacing.SOUTH},
-            {EnumFacing.EAST},
-            {EnumFacing.WEST}
+        // adjacent
+        firstTierPaths[0],
+        firstTierPaths[1],
+        {EnumFacing.NORTH},
+        {EnumFacing.SOUTH},
+        {EnumFacing.EAST},
+        {EnumFacing.WEST}
     };
 
     /**
      * The paths for felling three, to reach adjacent blocks including diagonals.
      */
     private static final EnumFacing[][] thirdTierPaths = {
-            // adjacent
-            secondTierPaths[0],
-            secondTierPaths[1],
-            secondTierPaths[2],
-            secondTierPaths[3],
-            secondTierPaths[4],
-            secondTierPaths[5],
-            // corners
-            {EnumFacing.NORTH, EnumFacing.WEST},
-            {EnumFacing.NORTH, EnumFacing.EAST},
-            {EnumFacing.SOUTH, EnumFacing.WEST},
-            {EnumFacing.SOUTH, EnumFacing.EAST},
-            // top/bottom corners
-            {EnumFacing.UP, EnumFacing.NORTH, EnumFacing.WEST},
-            {EnumFacing.UP, EnumFacing.NORTH, EnumFacing.EAST},
-            {EnumFacing.UP, EnumFacing.SOUTH, EnumFacing.WEST},
-            {EnumFacing.UP, EnumFacing.SOUTH, EnumFacing.EAST},
-            {EnumFacing.DOWN, EnumFacing.NORTH, EnumFacing.WEST},
-            {EnumFacing.DOWN, EnumFacing.NORTH, EnumFacing.EAST},
-            {EnumFacing.DOWN, EnumFacing.SOUTH, EnumFacing.WEST},
-            {EnumFacing.DOWN, EnumFacing.SOUTH, EnumFacing.EAST},
+        // adjacent
+        secondTierPaths[0],
+        secondTierPaths[1],
+        secondTierPaths[2],
+        secondTierPaths[3],
+        secondTierPaths[4],
+        secondTierPaths[5],
+        // corners
+        {EnumFacing.NORTH, EnumFacing.WEST},
+        {EnumFacing.NORTH, EnumFacing.EAST},
+        {EnumFacing.SOUTH, EnumFacing.WEST},
+        {EnumFacing.SOUTH, EnumFacing.EAST},
+        // top/bottom corners
+        {EnumFacing.UP, EnumFacing.NORTH, EnumFacing.WEST},
+        {EnumFacing.UP, EnumFacing.NORTH, EnumFacing.EAST},
+        {EnumFacing.UP, EnumFacing.SOUTH, EnumFacing.WEST},
+        {EnumFacing.UP, EnumFacing.SOUTH, EnumFacing.EAST},
+        {EnumFacing.DOWN, EnumFacing.NORTH, EnumFacing.WEST},
+        {EnumFacing.DOWN, EnumFacing.NORTH, EnumFacing.EAST},
+        {EnumFacing.DOWN, EnumFacing.SOUTH, EnumFacing.WEST},
+        {EnumFacing.DOWN, EnumFacing.SOUTH, EnumFacing.EAST},
     };
 
     /**
      * Array of all the paths for each of the three felling levels.
      */
     private static final EnumFacing[][][] fellingPaths = {
-            firstTierPaths,
-            secondTierPaths,
-            thirdTierPaths,
+        firstTierPaths,
+        secondTierPaths,
+        thirdTierPaths,
     };
 
     /**
@@ -93,8 +91,8 @@ public class FellingAlgorithm {
      * each path continues felling on that block as well.
      *
      * @param blockPosition The position of the block to start on.
-     * @param world    The world.
-     * @param player   The player to start the algorithm.
+     * @param world         The world.
+     * @param player        The player to start the algorithm.
      * @return Whether the algorithm executed.
      */
     public static boolean fellingAlgorithm(BlockPos blockPosition, World world, EntityPlayer player) {
@@ -106,7 +104,11 @@ public class FellingAlgorithm {
         blocksToBreak.add(blockPosition, 0);
         int blocksBroken = 0;
 
-        while (!blocksToBreak.isEmpty() && (blocksBroken <= Configuration.serverSide.maxBlocks || Configuration.serverSide.maxBlocks == 0)) {
+        while (
+            !blocksToBreak.isEmpty() &&
+            (blocksBroken < Configuration.serverSide.maxBlocks || Configuration.serverSide.maxBlocks == 0) &&
+            player.getHeldItemMainhand() != ItemStack.EMPTY
+        ) {
             blockPosition = blocksToBreak.peek(); // next block
             int distance = blocksToBreak.getValue(blockPosition);
             blocksToBreak.remove();
@@ -114,9 +116,14 @@ public class FellingAlgorithm {
             if (distance == 0) {
                 breakBlock(blockPosition, world, player); // is a log, break
                 blocksBroken += 1;
-            }
 
-            if (mainHandBreaksWhenDamaged(player, getTreePart(world, blockPosition))) return;
+                player.getHeldItemMainhand().damageItem(
+                    getTreePart(world, blockPosition) == TreePart.LOG ?
+                        Configuration.serverSide.durabilityDamage :
+                        Configuration.serverSide.durabilityDamage * Configuration.serverSide.leafMultiplier / 100,
+                    player
+                );
+            }
 
             for (EnumFacing[] pathToFollow : paths) {
                 BlockPos nextBlockPosition = travelToBlock(blockPosition, pathToFollow);
@@ -171,47 +178,20 @@ public class FellingAlgorithm {
 
             world.getBlockState(blockPosition).getBlock().dropBlockAsItem(world, blockPosition, world.getBlockState(blockPosition), fortune); // drop the block
             world.getBlockState(blockPosition).getBlock().dropXpOnBlockBreak( // drop the xp
+                world,
+                blockPosition,
+                world.getBlockState(blockPosition).getBlock().getExpDrop(
+                    world.getBlockState(blockPosition),
                     world,
                     blockPosition,
-                    world.getBlockState(blockPosition).getBlock().getExpDrop(
-                            world.getBlockState(blockPosition),
-                            world,
-                            blockPosition,
-                            fortune
-                    )
+                    fortune
+                )
             );
         }
 
         world.setBlockToAir(blockPosition); // delete the block
     }
 
-    /**
-     * Deals damage to the enchant and breaks if needed.
-     *
-     * @param thePlayer The player.
-     * @param treePart  The part being broken.
-     * @return Whether the tool was broken.
-     */
-    private static boolean mainHandBreaksWhenDamaged(EntityPlayer thePlayer, TreePart treePart) {
-        if (thePlayer.isCreative()) return false;
-        if (!toolBreaksWhenDamaged((EntityPlayerMP) thePlayer, thePlayer.getHeldItemMainhand(), treePart)) return false;
-
-        thePlayer.inventory.deleteStack(thePlayer.getHeldItemMainhand());
-        return true;
-    }
-
-    /**
-     * Checks all the blocks reached from the list of paths and checks
-     * if they are valid breaks before returning the lost of all valid.
-     *
-     * @param blocks          The block queue to add valid blocks to.
-     * @param world           The world
-     * @param paths           The array of paths to get to the valid blocks.
-     * @param currentDistance The current distance.
-     */
-    private static void getSurroundingBlocks(ValueUniqueQueue<BlockPos> blocks, World world, EnumFacing[][] paths, int currentDistance) {
-
-    }
 
     /**
      * Gets the server side config to see if leaf cutting is enabled
@@ -222,23 +202,6 @@ public class FellingAlgorithm {
      */
     private static boolean shouldBreakTreePart(TreePart treePart) {
         return Configuration.serverSide.cutLeaves ? treePart == TreePart.LOG || treePart == TreePart.LEAF : treePart == TreePart.LOG;
-    }
-
-    /**
-     * Damages the given item and returns whether it should break.
-     *
-     * @param thePlayer The player to deal damage to.
-     * @param theTool   The tool to deal damage to.
-     * @param treePart  The part of the tree.
-     * @return Whether the tool breaks.
-     */
-    private static boolean toolBreaksWhenDamaged(EntityPlayerMP thePlayer, ItemStack theTool, TreePart treePart) {
-        return theTool.attemptDamageItem(
-                treePart == TreePart.LEAF ?
-                        Configuration.serverSide.durabilityDamage * Configuration.serverSide.leafMultiplier / 100 :
-                        Configuration.serverSide.durabilityDamage,
-                new Random(),
-                thePlayer);
     }
 
     /**
